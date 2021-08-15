3bacebf3332348dca690a5d07e3b42ef7672c636
ComputerScientist-01 <rathi406@gmail.com>
2021-08-15 20:06:57 +0530

    📦 NEW: creating the array

--- sorting.py
+++ sorting.py
@@ -2,3 +2,12 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+N = 30
+
+# creating an array and rounding it off
+arr = np.round(np.linspace(1, 1000, N), 0)
+
+# shuffling the array
+np.random.shuffle(arr)
+print(arr)
