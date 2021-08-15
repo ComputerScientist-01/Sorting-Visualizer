d637b29a72764ab5f1b38e66d7e7c9c29b0c96f1
ComputerScientist-01 <rathi406@gmail.com>
2021-08-15 23:58:18 +0530

    📦 NEW: bar graph for visualization

--- sorting.py
+++ sorting.py
@@ -1,7 +1,9 @@
 import numpy as np
+from numpy.random import default_rng
 import scipy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+# matplotlib.use('Agg')
 
 N = 30
 
@@ -11,6 +13,11 @@ arr = np.round(np.linspace(1, 1000, N), 0)
 # shuffling the array
 np.random.shuffle(arr)
 print(arr)
+
+
+fig, ax = plt.subplots(figsize=(16, 8))
+container = ax.bar(np.arange(0, len(arr), 1),
+                   align="edge", width=0.8, height=arr)
 
 
 # insertion sort
@@ -25,3 +32,8 @@ for i in range(1, len(arr)):
 
 print("\n after sorting: \n")
 print(arr)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+container = ax.bar(np.arange(0, len(arr), 1),
+                   align="edge", width=0.8, height=arr)
+plt.show()
