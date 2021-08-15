d6cce115710e6b83f8bfebc8c7e82c3994856098
ComputerScientist-01 <rathi406@gmail.com>
2021-08-15 20:48:08 +0530

    📦 NEW: inserion sort

--- sorting.py
+++ sorting.py
@@ -11,3 +11,17 @@ arr = np.round(np.linspace(1, 1000, N), 0)
 # shuffling the array
 np.random.shuffle(arr)
 print(arr)
+
+
+# insertion sort
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i-1
+    while ((j >= 0) and (arr[j] > key)):
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+
+print("\n after sorting: \n")
+print(arr)
