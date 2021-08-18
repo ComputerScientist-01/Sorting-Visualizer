06414fe6ee52fe2fae861148fa4c2cbcc95fdb92
ComputerScientist-01 <rathi406@gmail.com>
2021-08-18 17:42:10 +0530

    NEW: Implemented quicksort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import partition
 from numpy.random import default_rng
 import scipy
 import matplotlib.pyplot as plt
@@ -26,17 +27,43 @@ container = ax.bar(np.arange(0, len(arr), 1),
 
 # insertion sort
 
-for i in range(1, len(arr)):
-    key = arr[i]
-    j = i-1
-    while ((j >= 0) and (arr[j] > key)):
-        arr[j+1] = arr[j]
-        j -= 1
-    arr[j+1] = key
+# for i in range(1, len(arr)):
+#     key = arr[i]
+#     j = i-1
+#     while ((j >= 0) and (arr[j] > key)):
+#         arr[j+1] = arr[j]
+#         j -= 1
+#     arr[j+1] = key
 
-print("\n after sorting: \n")
+# print("\n after sorting: \n")
+# print(arr)
+
+# quick sort
+
+def quicksort(arr, lo, hi):
+    if(lo < hi):
+        p = partition(arr, lo, hi)
+        quicksort(arr, lo, p-1)
+        quicksort(arr, p+1, hi)
+
+
+def partition(arr, lo, hi):
+    pivot = arr[hi]
+    i = lo
+    for j in range(lo, hi):
+        if arr[j] < pivot:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i += 1
+    temp = arr[i]
+    arr[i] = arr[hi]
+    arr[hi] = temp
+    return i
+
+
+quicksort(arr, 0, len(arr)-1)
 print(arr)
-
 fig, ax = plt.subplots(figsize=(16, 8))
 container = ax.bar(np.arange(0, len(arr), 1),
                    align="edge", width=0.8, height=arr)
