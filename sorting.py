0d1ebc2cf420a4a9ffbb467aa7af0c53bccd46fe
ComputerScientist-01 <rathi406@gmail.com>
2021-08-21 13:17:02 +0530

    ADD : class to keep track of the array

--- sorting.py
+++ sorting.py
@@ -4,7 +4,40 @@ from numpy.random import default_rng
 import scipy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-# matplotlib.use('Agg')
+
+
+class TrackedArray():
+    def __init__(self, arr):
+        self.arr = np.copy(arr)
+        self.reset()
+
+    def reset(self):
+        self.indices = []
+        self.values = []
+        self.access_type = []
+        self.full_copies = []
+
+    def __getitem__(self, key):
+        self.track(key, "get")
+        return self.arr.__getitem__(key)
+
+    def __setitem__(self, key, value):
+        self.arr.__setitem__(key, value)
+        self.track(key, "set")
+
+    def __delitem__(self, key):
+        self.track(key, "del")
+        self.arr.__delitem__(key)
+
+    def __len__(self):
+        return self.arr.__len__()
+
+    def __str__(self):
+        return self.arr.__str__()
+
+    def __repr__(self):
+        return self.arr.__repr__()
+
 
 N = 30
 
